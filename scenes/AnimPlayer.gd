5232211128918296a4280184e92e1a7b1b173e86
pietra renner <pietra.renner@gmail.com>
2024-06-02 17:05:45 -0300

    mundando o cenário

--- scenes/AnimPlayer.gd
+++ scenes/AnimPlayer.gd
@@ -3,7 +3,7 @@ class_name Player
 
 @export var speed = 300.0
 @export var lives := 5
-@export var jump_speed := -700.0
+@export var jump_speed := -850.0
 @export var gravity := 2500.0
 @onready var sprite = $PlayerSprite
 @onready var jumpsound := $JumpSound
@@ -48,17 +48,20 @@ func _physics_process(delta):
 	move_side(delta)
 
 func _on_hurt_box_body_entered(body: Node2D) -> void:
-	if ray_cast_right.is_colliding():
-		take_damage(Vector2(-500, 500))
-	elif ray_cast_left.is_colliding():
-		take_damage(Vector2(500, 500))
+	#if ray_cast_right.is_colliding():
+		#take_damage(Vector2(-500, 500))
+	#elif ray_cast_left.is_colliding():
+		#take_damage(Vector2(500, 500))
 	#get_tree().call_group("hud", "updateLives")
-
-func take_damage(knockback_force := Vector2.ZERO, duration := 0.25):
-	if knockback_force != Vector2.ZERO:
-		knockback_vector = knockback_force
-		
 	var knockback_tween := get_tree().create_tween()
-	knockback_tween.tween_property(self, "knockback_vector", Vector2.ZERO, duration)
 	sprite.modulate = Color(1,0,0,1)
-	knockback_tween.tween_property(sprite, "modulate", Color(1,1,1,1), duration)
+	knockback_tween.tween_property(sprite, "modulate", Color(1,1,1,1), 0.25)
+
+#func take_damage(knockback_force := Vector2.ZERO, duration := 0.25):
+	#if knockback_force != Vector2.ZERO:
+		#knockback_vector = knockback_force
+		
+	#var knockback_tween := get_tree().create_tween()
+	#knockback_tween.tween_property(self, "knockback_vector", Vector2.ZERO, duration)
+	#sprite.modulate = Color(1,0,0,1)
+	#knockback_tween.tween_property(sprite, "modulate", Color(1,1,1,1), duration)
--- scenes/HitBox.gd
+++ scenes/HitBox.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@export var sprite : AnimatedSprite2D
+
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
 		get_tree().call_group("hud", "updateLives")
