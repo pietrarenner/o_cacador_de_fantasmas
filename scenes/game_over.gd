f1cded6f48b57337e91cb56b469f6daaa624a171
carolina-kowaluk <carolina.kowaluk@gmail.com>
2024-06-28 15:27:03 -0300

    restart e exit

--- scenes/game_over.gd
+++ scenes/game_over.gd
@@ -5,9 +5,13 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	diedsound.play()
+	$ExitGame.connect("pressed", Callable(self, "_on_quit_button_pressed"))
 	
 func _physics_process(delta: float) -> void:
 	var restart := Input.is_action_just_pressed("restart")
-	#if(restart):
-		#get_tree().change_scene_to_file("")
-		#print("oi")
+	if(restart):
+		get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+
