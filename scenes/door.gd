1a0bb63434492bfa0600e48799957b8a65359634
pietra renner <pietra.renner@gmail.com>
2024-07-07 15:24:38 -0300

    o que fazer na porta

--- scenes/door.gd
+++ scenes/door.gd
@@ -3,16 +3,18 @@ extends StaticBody2D
 @onready var sprite = $AnimatedSprite2D
 var isOnDoor := false
 @onready var doorSound := $DoorSound
+@onready var label := $Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	label.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if isOnDoor and Input.is_action_just_pressed("restart_next_phase"):
-		get_tree().call_group("door", "nextPhase")
-		isOnDoor = false
+	if isOnDoor:
+		if Input.is_action_just_pressed("restart_next_phase"):
+			get_tree().call_group("door", "nextPhase")
+			#isOnDoor = false
 	
 func open():
 	sprite.play("opening")
@@ -22,4 +24,11 @@ func open():
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	#mostrar mensagem para a pessoa pressionar E para passar de fase
 	if body is Player:
+		label.visible = true
 		isOnDoor = true
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body is Player:
+		label.visible = false
+		isOnDoor = false
